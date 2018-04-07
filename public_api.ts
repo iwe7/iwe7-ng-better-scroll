e3175d7d3e6d9e0a22a3cc8197937dd52dd1bff3
iwe7 <admin@meepo.com.cn>
2018-04-08 07:15:20 +0800

    push to save

--- public_api.ts
+++ public_api.ts
@@ -1,4 +1,18 @@
 export { NgBetterScrollModule } from "./ng-better-scroll.module";
 export {
-  BetterScrollConfigDefault
+  BetterScrollConfigDefault,
+  BetterScrollComponent,
+  BETTER_SCROLL_CONFIG
 } from "./better-scroll/better-scroll.component";
+
+// insterface
+export {
+  BetterScrollWhellInterface,
+  BetterScrollEasingInterface,
+  BetterScrollSnapInterface,
+  BetterScrollScrollbarInterface,
+  BetterScrollPullDownRefreshInterface,
+  BetterScrollPullUpLoadInterface,
+  BetterScrollMouseWheelInterface
+} from "./better-scroll/better-scroll.component";
+
