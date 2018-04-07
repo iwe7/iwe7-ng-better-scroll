d816cc632516df1a3b5b9d8af5d1d8d93d69ff3a
iwe7 <admin@meepo.com.cn>
2018-04-07 22:01:08 +0800

    first commit

---
+++ ng-better-scroll.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  BetterScrollComponent,
+  BETTER_SCROLL_CONFIG
+} from "./better-scroll/better-scroll.component";
+import { ObserversModule } from "@angular/cdk/observers";
+@NgModule({
+  imports: [CommonModule, ObserversModule],
+  declarations: [BetterScrollComponent],
+  exports: [BetterScrollComponent]
+})
+export class NgBetterScrollModule {
+  public static forRoot(cfg: any): ModuleWithProviders {
+    return {
+      ngModule: NgBetterScrollModule,
+      providers: [
+        {
+          provide: BETTER_SCROLL_CONFIG,
+          useValue: cfg
+        }
+      ]
+    };
+  }
+}
---
+++ public_api.ts
@@ -0,0 +1,4 @@
+export { NgBetterScrollModule } from "./ng-better-scroll.module";
+export {
+  BetterScrollConfigDefault
+} from "./better-scroll/better-scroll.component";
